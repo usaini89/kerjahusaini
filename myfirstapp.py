a92faf1da3ab7fb459b5dbfff1a4ccefeb0505dd
usaini89 <154412092+usaini89@users.noreply.github.com>
2023-12-20 16:35:45 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -13,16 +13,26 @@ if option=='line chart':
       np.random.randn(20, 3),
       columns=['a', 'b', 'c'])
     st.line_chart(chart_data)
+    
 elif option=='map':
     map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [3.1242, 101.6861],
     columns=['lat', 'lon'])
     st.map(map_data)
 
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
-if show:
-  st.write(pd.DataFrame({
-    'Students': ['John', 'Lofa', 'Siti', 'Amy'],
-    'Attendance Status': ['yes', 'yes', 'yes', 'no']
-}))
+elif option=='T n C':
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+          'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+          'Attendance Status': ['yes', 'yes', 'yes', 'no']
+        }))
+
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+          st.write(pd.DataFrame({
+            'Students': ['John', 'Lofa', 'Siti', 'Amy'],
+            'Attendance Status': ['yes', 'yes', 'yes', 'no']
+            }))
